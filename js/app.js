b5520d0fe62a816f22735d83577f441288686642
ekatheryni <fedorenkokate98@gmail.com>
2018-04-22 18:16:53 +0300

    Commit_for_Masha

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
 jQuery(document).ready(function () {
     'use strict';
-    jQuery('#filter-date, #search-from-date, #search-to-date').datetimepicker();
+    jQuery('#filter-date').datetimepicker();
+
+    $('#submit-service').click(oknoAppears);
+
+    var $CLIENT_TEMPLATE = $('.login-box').html();
+
+    var $CLIENT_EMAIL = $('#client-email');
+    var $CLIENT_CODE = $('#client-code');
+    var $CLIENT_DATE = $('#filter-date');
+    var $CLIENT_NAME = $('#client-name');
+
+    var $NAME_INPUT = $('.client-name-submit');
+
+    function oknoAppears() {
+        var client_name = $CLIENT_NAME.val();
+        var client_date = $CLIENT_DATE.getDate()
+        var client_time = $CLIENT_DATE.getTime();
+    }
+
 });
+
