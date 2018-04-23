0332c129b2d03e1cd45a8872b0e219f24a1fc0b5
MMatveieva <matveieva.mariya@gmail.com>
2018-04-24 01:19:27 +0300

    ajax..

--- js/app.js
+++ js/app.js
@@ -81,5 +81,37 @@ $(function () {
 
     // SCROLL BUTTONS END!
 
+    // API
+    var API_URL = "http://localhost:5050";
+
+    function backendGet(url, callback) {
+        $.ajax({
+            url: API_URL + url,
+            type: 'GET',
+            success: function(data){
+                callback(null, data);
+            },
+            error: function() {
+                callback(new Error("Ajax Failed"));
+            }
+        })
+    }
+
+    function backendPost(url, data, callback) {
+        $.ajax({
+            url: API_URL + url,
+            type: 'POST',
+            contentType : 'application/json',
+            data: JSON.stringify(data),
+            success: function(data){
+                callback(null, data);
+            },
+            error: function() {
+                callback(new Error("Ajax Failed"));
+            }
+        })
+    }
+    // API END!
+
 });
 
